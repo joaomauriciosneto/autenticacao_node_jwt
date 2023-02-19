e8b855c48ba763ce58036b4a6e32617e90650b76
João Maurício <joaomauricio1975@gmail.com>
2023-02-19 20:35:25 -0300

    criação da conexão com o banco de dados

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ app.get('/', (req, res) => {
     })
 })
 
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+mongoose.set("strictQuery", true)
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.du4fqip.mongodb.net/?retryWrites=true&w=majority`).then(() => {
+    console.log('Conectou ao banco!');
+}).catch((err) => console.log(err))
+
 app.listen(porta, () => {
     console.log(`Server running at port: ${porta}`);
 })
